ff20c36b644d1ccaa65ad8b0985391206996ab98
Samuel W. Flint <swflint@flintfam.org>
2024-02-21 18:29:37 -0600

    Organize code to make navigation easier

--- consult-reftex.el
+++ consult-reftex.el
@@ -52,7 +52,9 @@
   :group 'consult-reftex
   :type '(repeat (string :tag "Command")))
 
+
 ;; Embark integration
+
 (with-eval-after-load 'embark
   (defvar consult-reftex-label-map
     (let ((map (make-sparse-keymap)))
@@ -69,17 +71,8 @@
   (add-to-list 'embark-exporters-alist '(reftex-label . consult-reftex-embark-export))
   (add-to-list 'embark-keymap-alist '(reftex-label . consult-reftex-label-map)))
 
-(defun consult-reftex-label-candidates (prefix)
-  "Find all references in current document (multi-file) using reftex.
-
-With prefix arg PREFIX, rescan the document for references."
-  (reftex-access-scan-info prefix)
-  ;; (when (equal prefix 4) (reftex-parse-all))
-  (let ((all-candidates))
-    (dolist (entry (symbol-value reftex-docstruct-symbol) all-candidates)
-      (when (stringp (car entry))
-        (push (consult-reftex--make-annotation (car entry) (nth 2 entry) (nth 3 entry) (cadr entry))
-              (alist-get (cadr entry) all-candidates nil nil 'string=))))))
+
+;; Internal Functions
 
 (defun consult-reftex--compile-categories ()
   (let ((styles-available (reftex-uniquify-by-car
@@ -111,7 +104,80 @@ With prefix arg PREFIX, rescan the document for references."
                                                       categories-alist :key #'car))))
       (sort new-categories-alist (lambda (a b) (string< (downcase (cdr a)) (downcase (cdr b))))))))
 
-(defun consult-reftex--reference (&optional arg)
+(defun consult-reftex-active-styles ()
+  "Determine active reference styles."
+  (apply #'append
+         (mapcar (lambda (style)
+                   (cadr (alist-get style reftex-ref-style-alist
+                                    nil nil #'equal)))
+                 (reftex-ref-style-list))))
+
+(defun consult-reftex--find-preferred-command (available-styles)
+  "Find a preferred style from AVAILABLE-STYLES."
+  (let ((out-commands (list)))
+    (dolist (command consult-reftex-preferred-style-order (or (car (reverse out-commands)) "\\ref"))
+      (when (cl-member command available-styles :test #'string= :key #'car)
+        (push command out-commands)))))
+
+(defun consult-reftex--label-marker (label file open-fn)
+  "Return marker corresponding to LABEL location in FILE.
+
+File is opened as necessary with OPEN-FN."
+  (let ((backward t) found buffer marker)
+    (setq buffer (funcall open-fn file))
+    (setq re (format reftex-find-label-regexp-format (regexp-quote label)))
+    (with-current-buffer buffer
+      (save-excursion
+        (setq found
+              (if backward
+                  (re-search-backward re nil t)
+                (re-search-forward re nil t)))
+        (unless found
+          (goto-char (point-min))
+          (unless (setq found (re-search-forward re nil t))
+            ;; Ooops.  Must be in a macro with distributed args.
+            (setq found
+                  (re-search-forward
+                   (format reftex-find-label-regexp-format2
+                           (regexp-quote label))
+                   nil t))))))
+    (if (match-end 3)
+        (setq marker (set-marker (make-marker) (match-beginning 3) buffer)))))
+
+
+;; Labelling/Annotation
+
+(defun consult-reftex-label-candidates (prefix)
+  "Find all references in current document (multi-file) using reftex.
+
+With prefix arg PREFIX, rescan the document for references."
+  (reftex-access-scan-info prefix)
+  ;; (when (equal prefix 4) (reftex-parse-all))
+  (let ((all-candidates))
+    (dolist (entry (symbol-value reftex-docstruct-symbol) all-candidates)
+      (when (stringp (car entry))
+        (push (consult-reftex--make-annotation (car entry) (nth 2 entry) (nth 3 entry) (cadr entry))
+              (alist-get (cadr entry) all-candidates nil nil 'string=))))))
+
+(defun consult-reftex--make-annotation (key annotation file type)
+  "Annotate KEY with ANNOTATION, TYPE and FILE if the latter is not nil."
+  (cond
+   ((not annotation) key)
+   (t (propertize key 'reftex-annotation annotation
+                  'reftex-file       file
+                  'reftex-type       type))))
+
+(defun consult-reftex--get-annotation (cand)
+  "Get the annotation for CAND."
+  (when-let ((ann (get-text-property 0 'reftex-annotation cand)))
+    (concat (propertize " " 'display '(space :align-to center)) ann)))
+
+
+;; Reference Manipulation
+
+(defvar consult-reftex--reference-history nil)
+
+(defun consult-reftex-select-reference (&optional arg)
   "Select a label with consult-based completing-read.
 
 If ARG, force a reparse for label candidates."
@@ -139,61 +205,6 @@ If ARG, force a reparse for label candidates."
                              :annotate #'consult-reftex--get-annotation)))))
     label))
 
-(defun consult-reftex--get-annotation (cand)
-  "Get the annotation for CAND."
-  (when-let ((ann (get-text-property 0 'reftex-annotation cand)))
-    (concat (propertize " " 'display '(space :align-to center)) ann)))
-
-(defvar consult-reftex--reference-history nil)
-
-(defun consult-reftex--make-annotation (key annotation file type)
-  "Annotate KEY with ANNOTATION, TYPE and FILE if the latter is not nil."
-  (cond
-   ((not annotation) key)
-   (t (propertize key 'reftex-annotation annotation
-                  'reftex-file       file
-                  'reftex-type       type))))
-
-(defun consult-reftex--label-marker (label file open-fn)
-  "Return marker corresponding to LABEL location in FILE.
-
-File is opened as necessary with OPEN-FN."
-  (let ((backward t) found buffer marker)
-    (setq buffer (funcall open-fn file))
-    (setq re (format reftex-find-label-regexp-format (regexp-quote label)))
-    (with-current-buffer buffer
-      (save-excursion
-        (setq found
-              (if backward
-                  (re-search-backward re nil t)
-                (re-search-forward re nil t)))
-        (unless found
-          (goto-char (point-min))
-          (unless (setq found (re-search-forward re nil t))
-            ;; Ooops.  Must be in a macro with distributed args.
-            (setq found
-                  (re-search-forward
-                   (format reftex-find-label-regexp-format2
-                           (regexp-quote label))
-                   nil t))))))
-    (if (match-end 3)
-        (setq marker (set-marker (make-marker) (match-beginning 3) buffer)))))
-
-(defun consult-reftex-active-styles ()
-  "Determine active reference styles."
-  (apply #'append
-         (mapcar (lambda (style)
-                   (cadr (alist-get style reftex-ref-style-alist
-                                    nil nil #'equal)))
-                 (reftex-ref-style-list))))
-
-(defun consult-reftex--find-preferred-command (available-styles)
-  "Find a preferred style from AVAILABLE-STYLES."
-  (let ((out-commands (list)))
-    (dolist (command consult-reftex-preferred-style-order (or (car (reverse out-commands)) "\\ref"))
-      (when (cl-member command available-styles :test #'string= :key #'car)
-        (push command out-commands)))))
-
 ;;;###autoload
 (defun consult-reftex-insert-reference (&optional arg no-insert)
   "Insert reference with completion.
@@ -201,7 +212,7 @@ File is opened as necessary with OPEN-FN."
 With prefix ARG rescan the document.  If NO-INSERT, only format
 the reference."
   (interactive "P")
-  (when-let* ((label (consult-reftex--reference arg))
+  (when-let* ((label (consult-reftex-select-reference arg))
               (active-styles (consult-reftex-active-styles))
               (default-style (consult-reftex--find-preferred-command active-styles))
               (reference
@@ -225,7 +236,7 @@ the reference."
 ;;;###autoload
 (defun consult-reftex-goto-label (label &optional _arg)
   "Select LABEL using Consult and jump to it."
-  (interactive (list (consult-reftex--reference current-prefix-arg)
+  (interactive (list (consult-reftex-select-reference current-prefix-arg)
                      current-prefix-arg))
   (if-let* ((open (consult--temporary-files))
             (marker (consult-reftex--label-marker (substring-no-properties label)
